80c2356270a78c43ca506b8bda63df1622ca629f
aEnigmatic <aEnigmatic@users.noreply.github.com>
2019-08-29 16:19:16 +0200

    Update

--- src/Sol/FFBE/Reader/MstReader.php
+++ src/Sol/FFBE/Reader/MstReader.php
@@ -35,29 +35,31 @@
             // un-indent arrays
             $keys = ['effect_frames', 'attack_damage', 'attack_frames', 'effects_raw', 'levels', 'requirements', 'roles'];
             $keys = array_map('preg_quote', $keys);
-            $keys = join('|', $keys);
+            // $keys = join('|', $keys);
 
-            $data = preg_replace_callback('~("(?:' . $keys . ')":\s+)\[\s+(?P<values>(?:[-.\d]+|"[^"]*"|\[]|\[\s*(?P>values)\s*])(?:,\s+(?P>values)\s*)*)\s*]~',
-                function ($match) {
-                    $trimmed = preg_replace('~\r?\n\s+~', '', $match[2]);
-                    $trimmed = str_replace(',', ', ', $trimmed);
+            foreach ($keys as $key) {
+                $data = preg_replace_callback('~("(?:' . $key . ')":\s+)\[\s+(?P<values>(?:[-.\d]+|"[^"]*"|\[]|\[\s*(?P>values)\s*])(?:,\s+(?P>values)\s*)*)\s*]~',
+                    function ($match) {
+                        $trimmed = preg_replace('~\r?\n\s+~', '', $match[2]);
+                        $trimmed = str_replace(',', ', ', $trimmed);
 
-                    return "{$match[1]}[{$trimmed}]";
-                }, $data);
+                        return "{$match[1]}[{$trimmed}]";
+                    }, $data);
 
-            $errs = [
-                PREG_NO_ERROR              => 'No error',
-                PREG_INTERNAL_ERROR        => 'Internal error',
-                PREG_BACKTRACK_LIMIT_ERROR => 'Backtrack limit reached error',
-                PREG_RECURSION_LIMIT_ERROR => 'Recursion limit reached error',
-                PREG_BAD_UTF8_ERROR        => 'Bad UTF-8 error',
-                PREG_BAD_UTF8_OFFSET_ERROR => 'Bad UTF-8 offset error',
-                PREG_JIT_STACKLIMIT_ERROR  => 'JIT stack limit reached error',
-            ];
+                $errs = [
+                    PREG_NO_ERROR              => 'No error',
+                    PREG_INTERNAL_ERROR        => 'Internal error',
+                    PREG_BACKTRACK_LIMIT_ERROR => 'Backtrack limit reached error',
+                    PREG_RECURSION_LIMIT_ERROR => 'Recursion limit reached error',
+                    PREG_BAD_UTF8_ERROR        => 'Bad UTF-8 error',
+                    PREG_BAD_UTF8_OFFSET_ERROR => 'Bad UTF-8 offset error',
+                    PREG_JIT_STACKLIMIT_ERROR  => 'JIT stack limit reached error',
+                ];
 
-            if (preg_last_error() > 0) {
-                var_dump(['preg_err', preg_last_error(), $errs[preg_last_error()]]);
-                die();
+                if (preg_last_error() > 0) {
+                    var_dump([preg_last_error(), $errs[preg_last_error()], $key]);
+                    die();
+                }
             }
 
             // un-indent objects
